16616bc2d8fb133235dcab72089067561fcedd43
Tom Koptel <tom.koptel@gmail.com>
2012-10-27 21:28:53 +0300

    Creating compound view

---
+++ src/com/paad/todolist/MyCompoundView.java
@@ -0,0 +1,45 @@
+package com.paad.todolist;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MyCompoundView extends LinearLayout implements Button.OnClickListener{
+    private TextView editText;
+    private Button clearButton;
+
+    public MyCompoundView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public MyCompoundView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public MyCompoundView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context);
+    }
+
+    private void init(Context context) {
+        String infService = context.LAYOUT_INFLATER_SERVICE;
+        LayoutInflater li;
+        li = (LayoutInflater) getContext().getSystemService(infService);
+        li.inflate(R.layout.clearable_edit_text, this, true);
+
+        editText = (TextView) findViewById(R.id.editText);
+        clearButton = (Button) findViewById(R.id.clearButton);
+        clearButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        editText.setText("");
+    }
+}
---
+++ src/com/paad/todolist/MyView.java
@@ -0,0 +1,74 @@
+package com.paad.todolist;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class MyView extends View{
+    public MyView(Context context) {
+        super(context);
+    }
+
+    public MyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int wMeasureSpec, int hMeasureSpec){
+        int measuredWidth = measureWidth(wMeasureSpec);
+        int measuredHeight = measureHeight(hMeasureSpec);
+
+        setMeasuredDimension(measuredWidth, measuredHeight);
+    }
+
+    private int measureWidth(int measureSpec) {
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+        int result = 500;
+
+        if(specMode == MeasureSpec.AT_MOST){
+            result = specSize;
+        }else if(specMode == MeasureSpec.EXACTLY){
+            result = specSize;
+        }
+        return result;
+    }
+
+    private int measureHeight(int measureSpec) {
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+        int result = 500;
+
+        if(specMode == MeasureSpec.AT_MOST){
+            result = specSize;
+        }else if(specMode == MeasureSpec.EXACTLY){
+            result = specSize;
+        }
+        return result;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas){
+        int height = getMeasuredHeight();
+        int width = getMeasuredWidth();
+
+        int px = width/2;
+        int py = height/2;
+
+        Paint mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mTextPaint.setColor(Color.WHITE);
+
+        String displayText = "Hello world";
+
+        float textWidth = mTextPaint.measureText(displayText);
+
+        canvas.drawText(displayText, px-textWidth/2, py, mTextPaint);
+    }
+}
