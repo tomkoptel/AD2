c7efa14896eb9cf38040081749f87345dc9a930a
Tom Koptel <tom.koptel@gmail.com>
2013-02-20 19:50:07 +0200

    Adding custom ContentProvider

--- src/com/paad/todolist/TodoListActivity.java
+++ src/com/paad/todolist/TodoListActivity.java
@@ -1,7 +1,13 @@
 package com.paad.todolist;
 
-import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.view.*;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,7 +17,8 @@ import com.paad.ad2.R;
 
 import java.util.ArrayList;
 
-public class TodoListActivity extends Activity implements EditText.OnKeyListener {
+public class TodoListActivity extends FragmentActivity implements EditText.OnKeyListener,
+        LoaderManager.LoaderCallbacks<Cursor> {
     private ArrayList<String> todoItems;
     private EditText myEditText;
     private ArrayAdapter<String> aa;
@@ -33,6 +40,8 @@ public class TodoListActivity extends Activity implements EditText.OnKeyListener
         myListView.setAdapter(aa);
 
         registerForContextMenu(myListView);
+
+        getSupportLoaderManager().initLoader(0, null, this);
     }
 
     @Override
@@ -124,6 +133,14 @@ public class TodoListActivity extends Activity implements EditText.OnKeyListener
         myEditText.requestFocus();
     }
 
+    public void onNewItemAdded(String newItem) {
+        ContentResolver contentResolver = getContentResolver();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(ToDoContentProvider.KEY_TASK, newItem);
+        contentResolver.insert(ToDoContentProvider.CONTENT_URI, contentValues);
+        getSupportLoaderManager().restartLoader(0, null, this);
+    }
+
     @Override
     public boolean onContextItemSelected(MenuItem item){
         super.onContextItemSelected(item);
@@ -141,4 +158,28 @@ public class TodoListActivity extends Activity implements EditText.OnKeyListener
         return false;
     }
 
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle bundle) {
+        return new CursorLoader(this, ToDoContentProvider.CONTENT_URI, null, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+        int keyTaskIndex = cursor.getColumnIndexOrThrow(ToDoContentProvider.KEY_TASK);
+        todoItems.clear();
+        while(cursor.moveToNext()) {
+//            ToDoItem newItem = new ToDoItem(cursor.getString(keyTaskIndex));
+//            todoItems.add(newItem);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> cursorLoader) {
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getSupportLoaderManager().restartLoader(0, null, this);
+    }
 }
